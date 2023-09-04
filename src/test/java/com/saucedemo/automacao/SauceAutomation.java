9a7ab30079c0e502ec8078366b5ad616e749591d
Andreza Oliveira <andrezaoliveira0593@gmail.com>
2023-09-04 11:00:19 -0400

    new cganges

--- src/test/java/com/saucedemo/automacao/SauceAutomation.java
+++ src/test/java/com/saucedemo/automacao/SauceAutomation.java
@@ -1,19 +1,38 @@
 package com.saucedemo.automacao;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Before;
-import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class SauceAutomation{
 
     ChromeDriver driver;
 
-    @Test
-    public void ValidaAutomation(){
+    @BeforeTest
+    public void inicializaAutomation() {
         WebDriverManager.chromedriver().setup();
 
         driver = new ChromeDriver();
+    }
+    @Test
+    public void validaAutomation() throws Exception {
+        driver.get("http://www.saucedemo.com");
+
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.name("login-button")).click();
+
+        Thread.sleep(5000);
+    }
+    @AfterTest
+    public void finalizaAutomation() {
+        driver.quit();
+
+
     }
 
 
